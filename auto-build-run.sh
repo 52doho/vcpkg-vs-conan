5b6445f2c0a75bc6b67840e4cc6426fa93113888
Guichao <52doho@gmail.com>
2020-05-08 18:23:34 +0800

    add vcpkg to implement library dependence

---
+++ auto-build-run.sh
@@ -0,0 +1,11 @@
+#/usr/bin/bash
+
+[ -d build ] || mkdir build
+cd build
+# NOTE: replace path
+cmake .. -DCMAKE_TOOLCHAIN_FILE=/home/jd/Downloads/SourceCode/vcpkg/scripts/buildsystems/vcpkg.cmake
+make -j 6
+
+echo ""
+echo "------> build success and running:"
+./ConanVCpkgDemo

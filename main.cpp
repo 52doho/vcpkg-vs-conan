dd8e978294828e9d677a0d2307867a37e791dec5
Guichao <52doho@gmail.com>
2020-05-08 12:20:02 +0800

    log cpprest and opencv version with glog

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <glog/logging.h>
+#include <cpprest/version.h>
+#include <opencv2/core/version.hpp>
+
+int main(int argc, char **argv) {
+    google::SetStderrLogging(google::INFO);
+    google::InitGoogleLogging(argv[0]);
+
+    LOG(INFO) << "from glog";
+    LOG(INFO) << "cpprest version: " << CPPREST_VERSION;
+    LOG(INFO) << "opencv version: " << CV_VERSION;
+
     return 0;
 }
